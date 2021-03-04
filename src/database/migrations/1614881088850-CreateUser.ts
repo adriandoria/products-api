949444d2e7c92dafc1c95833634ae405ba743cd1
Adrian Dória <adriandoria3@gmail.com>
2021-03-04 15:11:32 -0300

    Configuração do banco de dados

---
+++ src/database/migrations/1614881088850-CreateUser.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUser1614881088850 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
