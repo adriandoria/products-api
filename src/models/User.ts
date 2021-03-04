3deb1ea3e57eb0cac154dd743690908f5722d8b4
Guilherme Azevedo <guilhermeazevedo.dev@gmail.com>
2021-03-04 17:35:52 -0300

    O id de usuário agora é do tipo uuid (v4).

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,14 +1,29 @@
-import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm'
+import {Entity, Column, PrimaryGeneratedColumn, PrimaryColumn, CreateDateColumn} from 'typeorm'
+import {v4 as uuid} from 'uuid'
+/*
+- v4 = tipo de uuid.
+- as uuid = renomeia o v4 para uuid.
+*/
 
 @Entity()
-export class User {
-    
-    @PrimaryGeneratedColumn()
-    id: number
+class User {
+  @PrimaryColumn()
+  readonly id: String
 
-    @Column()
-    username: string
+  @Column()
+  name: String
 
-    @Column()
-    email: string
+  @Column()
+  email: String
+
+  @CreateDateColumn()
+  created_at: Date
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuid()
+    }
+  }
 }
+
+export{User}
