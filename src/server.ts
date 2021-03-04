83c43152022b63587afd9cef4d1f0180128ca527
Guilherme Azevedo <guilhermeazevedo.dev@gmail.com>
2021-03-04 18:18:40 -0300

    Arquivo agora apenas executa a api.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,3 @@
-import 'reflect-metadata'
-import express from 'express'
-import './database'
-
-const app = express()
+import { app } from './app'
 
 app.listen(3333, () => console.log('Server is running.'))
