3fd5d67c4735e6a1fef05c59466533505188c252
Guilherme Azevedo <guilhermeazevedo.dev@gmail.com>
2021-03-05 17:47:40 -0300

    Correção: Identação de código.

--- src/database/migrations/1614961684456-CreateProduct.ts
+++ src/database/migrations/1614961684456-CreateProduct.ts
@@ -3,35 +3,34 @@ import {MigrationInterface, QueryRunner, Table} from "typeorm";
 export class CreateProduct1614961684456 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-            new Table({
-              name: 'products',
-              columns: [
-                {
-                  name: 'id',
-                  type: 'uuid',
-                  isPrimary: true
-                },
-                {
-                  name: 'name',
-                  type: 'varchar',
-                },
-                {
-                  name: 'price',
-                  type: 'varchar'
-                },
-                {
-                    name: 'user_id',
-                    type: 'varchar',
-                },
-                {
-                  name: 'created_at',
-                  type: 'timestamp',
-                  default: 'now()'
-                }
-              ]
-            })
-        )
+      await queryRunner.createTable(
+        new Table({
+          name: 'products',
+          columns: [
+          {
+             name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'price',
+            type: 'varchar'
+          },
+          {
+            name: 'user_id',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+            }
+          ]
+      }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
