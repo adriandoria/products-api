c9784ba3f511636407a6f87a08b478e4ad69f103
Guilherme Azevedo <guilhermeazevedo.dev@gmail.com>
2021-03-06 00:01:52 -0300

    Configurações para rodar o Jest.

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,16 @@
-import {createConnection} from 'typeorm'
+import {Connection, createConnection, getConnectionOptions} from 'typeorm'
 
-createConnection()
+export default async (): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions() //Recebe as informações do ormconfig.json
+
+  //Para verificar se o comando dado foi yarn dev ou yarn test
+  return createConnection(
+    Object.assign(defaultOptions, {
+       /*
+      * Verfica qual variável de ambiente está sendo ultilizada.
+      * A aplicação usará o database de teste caso esteja sendo executada no ambiente de teste.
+      */
+      database: process.env.NODE_ENV === 'test' ? './src/database/database.test.sqlite' : defaultOptions.database
+    })
+  )
+}
